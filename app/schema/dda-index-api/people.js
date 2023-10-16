aed3185ae2604d959c8468c410b2e98b7e938cac
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-10-16 14:05:46 +0100

    Update address payload schema

--- app/schema/dda-index-api/people.js
+++ app/schema/dda-index-api/people.js
@@ -1,19 +1,20 @@
 const Joi = require('joi')
 
-const schema = Joi.array().items(
-  Joi.object({
-    title: Joi.string().required(),
-    first_name: Joi.string().required(),
-    last_name: Joi.string().required(),
-    address: Joi.object({
-      address_line_1: Joi.string().required(),
-      address_line_2: Joi.string().allow(null).optional(),
-      address_line_3: Joi.string().allow(null).optional(),
-      postcode: Joi.string().required(),
-      county: Joi.string().required(),
-      country: Joi.string().required()
-    })
-  })
-)
+const schema = Joi.object({
+  people: Joi.array().items(
+    Joi.object({
+      title: Joi.string().required(),
+      first_name: Joi.string().required(),
+      last_name: Joi.string().required(),
+      address: Joi.object({
+        address_line_1: Joi.string().required(),
+        address_line_2: Joi.string().allow(null).optional(),
+        address_line_3: Joi.string().allow(null).optional(),
+        postcode: Joi.string().required(),
+        county: Joi.string().required(),
+        country: Joi.string().required()
+      })
+    }))
+  }).required()
 
 module.exports = schema
