77eeee2332148da52df4d8dc8b9fd82062fc32f1
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-11-15 10:39:08 +0000

    Fix linting

--- test/unit/messaging/register-import.test.js
+++ test/unit/messaging/register-import.test.js
@@ -22,7 +22,7 @@ describe('register import message sender', () => {
         time: expect.any(String),
         subject: 'RegisterImport',
         dataContentType: 'text/json',
-        data: data
+        data
       },
       type: 'REIGSTER_IMPORT',
       source: 'aphw-ddi-portal'
