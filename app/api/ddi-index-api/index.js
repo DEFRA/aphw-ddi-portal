ab91fafbec7645df3b43f0b21bf1cd72a9597a59
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-12-01 14:14:47 +0000

    Revert "Fix linting"
    This reverts commit 6b68fa608e1f973642295a3e393131338f1f05bf.

--- app/api/ddi-index-api/index.js
+++ app/api/ddi-index-api/index.js
@@ -1,11 +1,13 @@
 const { getCounties } = require('./counties')
 const { getCountries } = require('./countries')
 const { getBreeds } = require('./dog-breeds')
+const { getCourts } = require('./courts')
 const { getPoliceForces } = require('./police-forces')
 
 module.exports = {
   getCounties,
   getCountries,
   getBreeds,
+  getCourts,
   getPoliceForces
 }
