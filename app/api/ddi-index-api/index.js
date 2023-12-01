6b68fa608e1f973642295a3e393131338f1f05bf
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-12-01 13:31:16 +0000

    Fix linting

--- app/api/ddi-index-api/index.js
+++ app/api/ddi-index-api/index.js
@@ -1,13 +1,11 @@
 const { getCounties } = require('./counties')
 const { getCountries } = require('./countries')
 const { getBreeds } = require('./dog-breeds')
-const { getCourts } = require('./courts')
 const { getPoliceForces } = require('./police-forces')
 
 module.exports = {
   getCounties,
   getCountries,
-  getBreeds
-  getCourts,
+  getBreeds,
   getPoliceForces
 }
