4d884b3ff4a278ad98bed8e3029da3619a469b01
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-12-04 15:57:25 +0000

    Add  button for extra dog

--- app/models/cdo/create/confirm-dog-details.js
+++ app/models/cdo/create/confirm-dog-details.js
@@ -6,7 +6,7 @@ function ViewModel(dogs) {
     formAction: routes.confirm.post,
     backLink: routes.details.get,
     dogs: dogs.map((dog, index) => ({
-      heading: `Dog ${index + 1}`,
+      id: index + 1,
       name: dog[keys.name],
       breed: dog[keys.breed],
       cdoIssued: formatToGds(dog[keys.cdoIssued]),
--- app/routes/cdo/create/confirm-dog-details.js
+++ app/routes/cdo/create/confirm-dog-details.js
@@ -2,7 +2,7 @@ const { routes, views } = require('../../../constants/cdo/dog')
 const { routes: cdoRoutes } = require('../../../constants/cdo')
 const { admin } = require('../../../auth/permissions')
 const ViewModel = require('../../../models/cdo/create/confirm-dog-details')
-const { getDogs } = require('../../../session/cdo/dog')
+const { getDogs, addAnotherDog } = require('../../../session/cdo/dog')
 const dogDetailsSchema = require('../../../schema/portal/cdo/dog-details')
 
 module.exports = [
@@ -23,10 +23,13 @@ module.exports = [
     path: routes.confirm.post,
     options: {
       auth: { scope: [admin] },
-      validate: {
-        payload: dogDetailsSchema
-      },
       handler: async (request, h) => {
+        if (request.payload['addAnotherDog'] != null) {
+          addAnotherDog(request)
+
+          return h.redirect(routes.details.get)
+        }
+
         return h.redirect(cdoRoutes.created.get)
       }
     }
