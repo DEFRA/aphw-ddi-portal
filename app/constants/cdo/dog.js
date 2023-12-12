3d8af45cf7b03e7d15d8f320ff277fe3c8b65473
sfitz42 <shaun.r.fitzsimons@gmail.com>
2023-12-12 09:22:46 +0000

    Add delete dog route

--- app/constants/cdo/dog.js
+++ app/constants/cdo/dog.js
@@ -7,11 +7,16 @@ const constants = {
     confirm: {
       get: '/cdo/create/confirm-dog-details',
       post: '/cdo/create/confirm-dog-details'
+    },
+    delete: {
+      get: '/cdo/create/confirm-dog-delete',
+      post: '/cdo/create/confirm-dog-delete'
     }
   },
   views: {
     details: 'cdo/create/dog-details',
-    confirm: 'cdo/create/confirm-dog-details'
+    confirm: 'cdo/create/confirm-dog-details',
+    delete: 'cdo/create/confirm-dog-delete'
   },
   keys: {
     entry: 'dogs',
