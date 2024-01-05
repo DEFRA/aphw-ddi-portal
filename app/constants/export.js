12eea431ca647442f2971787e18ace8a9a72f437
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2024-01-05 14:53:14 +0000

    Export data (#58)
    * Export data
    
    * Bumped version
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>

---
+++ app/constants/export.js
@@ -0,0 +1,13 @@
+const exportData = {
+  routes: {
+    export: {
+      get: '/export',
+      post: '/export'
+    }
+  },
+  views: {
+    export: 'export/export-data'
+  }
+}
+
+module.exports = exportData
