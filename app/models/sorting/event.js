fcc7a6a7af92fa7147218b69a8c49ed138fd509e
Chris Cole <whitewaterdesign>
2024-02-15 12:29:10 +0000

    feat: 298407 - Remove jsdoc

--- app/models/sorting/event.js
+++ app/models/sorting/event.js
@@ -1,23 +1,7 @@
-/**
- * @typedef Activity
- * @property {string} activityDate
- **/
-/**
-  * @typedef EventToSort
-  * @property {string} timestamp
-  * @property {'uk.gov.defra.ddi.event.update'|'uk.gov.defra.ddi.event.create'|'uk.gov.defra.ddi.event.activity'} type
-  * @property {Activity} [activity]
- **/
-
 const getUnixDate = (dateString) => {
   return new Date(dateString).getTime()
 }
 
-/**
- *
- * @param {EventToSort} a
- * @param {EventToSort} b
- */
 const sortEventsDescCompareFn = (a, b) => {
   const primarySortValueA = a.activity?.activityDate ? a.activity.activityDate : a.timestamp
   const primarySortValueB = b.activity?.activityDate ? b.activity.activityDate : b.timestamp
