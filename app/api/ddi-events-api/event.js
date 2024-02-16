cd3086c483b9e95441653ef2780b3a08003137db
Chris Cole <whitewaterdesign>
2024-02-16 10:16:52 +0000

    feat: 298407 - getEvents remove json option

--- app/api/ddi-events-api/event.js
+++ app/api/ddi-events-api/event.js
@@ -1,9 +1,7 @@
 const { get } = require('../ddi-events-api/base')
 
 const getEvents = async (indexNumbers) => {
-  return get(`events/?pks=${indexNumbers.join(',')}`, {
-    json: true
-  })
+  return get(`events/?pks=${indexNumbers.join(',')}`)
 }
 
 module.exports = {
