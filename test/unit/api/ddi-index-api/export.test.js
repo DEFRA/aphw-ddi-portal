ae0fcdb0a470470378736b1334cac70317217460
work <jeremy.barnsley@esynergy.co.uk>
2024-02-22 10:16:18 +0000

    Increased coverage

--- test/unit/api/ddi-index-api/export.test.js
+++ test/unit/api/ddi-index-api/export.test.js
@@ -4,7 +4,7 @@ describe('Export API endpoints', () => {
   jest.mock('../../../../app/api/ddi-index-api/base')
   const { get } = require('../../../../app/api/ddi-index-api/base')
 
-  const { exportData } = require('../../../../app/api/ddi-index-api/export')
+  const { exportData, triggerOvernight } = require('../../../../app/api/ddi-index-api/export')
 
   beforeEach(() => {
     jest.clearAllMocks()
@@ -16,5 +16,13 @@ describe('Export API endpoints', () => {
     await exportData(user)
 
     expect(get).toHaveBeenCalledWith('export', user)
+  })
+
+  test('triggerOvernight should call GET to API', async () => {
+    get.mockResolvedValue({ payload: { exported: {} } })
+
+    await triggerOvernight(user)
+
+    expect(get).toHaveBeenCalledWith('trigger-overnight', user)
   })
 })
