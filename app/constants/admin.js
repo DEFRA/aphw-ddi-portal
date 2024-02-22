8505cdd6cc21e749889d2d5edeb0c353d0b181dc
work <jeremy.barnsley@esynergy.co.uk>
2024-02-22 16:36:27 +0000

    Bumped and fixed tests

--- app/constants/admin.js
+++ app/constants/admin.js
@@ -5,33 +5,9 @@ const constants = {
     }
   },
   views: {
-    regularJobs: 'admin/regular-jobs',
-    viewDogDetails: 'cdo/view/dog-details',
-    viewDogActivities: 'cdo/view/check-activities',
-    editExemptionDetails: 'cdo/edit/exemption-details',
-    changeStatus: 'cdo/edit/change-status',
-    changeStatusConfirmation: 'cdo/edit/change-status-confirmation',
-    details: 'cdo/create/dog-details',
-    confirm: 'cdo/create/confirm-dog-details',
-    delete: 'cdo/create/confirm-dog-delete',
-    certificate: 'cdo/view/certificate',
-    addActivity: 'cdo/edit/add-activity',
-    selectActivity: 'cdo/edit/select-activity',
-    activityConfirmation: 'cdo/edit/activity-confirmation'
+    regularJobs: 'admin/regular-jobs'
   },
   keys: {
-    entry: 'dogs',
-    breed: 'breed',
-    name: 'name',
-    applicationType: 'applicationType',
-    interimExemption: 'interimExemption',
-    cdoIssued: 'cdoIssued',
-    cdoExpiry: 'cdoExpiry',
-    dateOfBirth: 'dateOfBirth',
-    dateOfDeath: 'dateOfDeath',
-    dateExported: 'dateExported',
-    dateStolen: 'dateStolen',
-    dateUntraceable: 'dateUntraceable'
   }
 }
 
