a664f76f1573aaad519a7a03cf48ff87db0584e5
Chris Cole <whitewaterdesign>
2024-02-26 12:44:19 +0000

    test: Contract test for event api

--- test/contract/aphw-ddi-api.test.js
+++ test/contract/aphw-ddi-api.test.js
@@ -1,56 +1,47 @@
-const path = require('path')
-const { Pact } = require('@pact-foundation/pact')
 const Matchers = require('@pact-foundation/pact/dsl/matchers')
+const { ddiIndexApiProvider } = require('./mockServices')
 let countriesApi
-let provider
 
-const consumerName = 'aphw-ddi-portal'
-const providerName = 'aphw-ddi-api'
-describe('Schedule contract test', () => {
-  beforeAll(async () => {
-    provider = new Pact({
-      consumer: consumerName,
-      provider: providerName,
-      port: 1234,
-      log: path.resolve(process.cwd(), 'test-output', 'pact.log'),
-      dir: path.resolve(process.cwd(), 'test-output')
+describe('API service contract test', () => {
+  describe('/countries', () => {
+    beforeAll(async () => {
+      const mockService = ddiIndexApiProvider
+      await mockService.setup()
+      jest.mock('../../app/config', () => ({
+        ddiIndexApi: mockService.mockService
+      }))
+      countriesApi = require('../../app/api/ddi-index-api/countries')
     })
-    await provider.setup()
-    const mockApiService = provider.mockService
-    jest.mock('../../app/config', () => ({
-      ddiIndexApi: mockApiService
-    }))
-    countriesApi = require('../../app/api/ddi-index-api/countries')
-  })
 
-  test('GET /countries', async () => {
-    await provider.addInteraction({
-      state: 'countries exist',
-      uponReceiving: 'get all countries',
-      withRequest: {
-        method: 'GET',
-        path: '/countries'
-      },
-      willRespondWith: {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json; charset=utf-8'
+    test('GET /countries', async () => {
+      await ddiIndexApiProvider.addInteraction({
+        state: 'countries exist',
+        uponReceiving: 'get all countries',
+        withRequest: {
+          method: 'GET',
+          path: '/countries'
         },
-        body: {
-          countries: Matchers.eachLike('England', null)
+        willRespondWith: {
+          status: 200,
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+          },
+          body: {
+            countries: Matchers.eachLike('England', null)
+          }
         }
-      }
-    })
+      })
 
-    const response = await countriesApi.getCountries()
-    expect(response[0]).toEqual('England')
+      const response = await countriesApi.getCountries()
+      expect(response[0]).toEqual('England')
+    })
   })
 
   afterEach(async () => {
-    await provider.verify()
+    await ddiIndexApiProvider.verify()
   })
 
   afterAll(async () => {
-    await provider.finalize()
+    await ddiIndexApiProvider.finalize()
   })
 })
