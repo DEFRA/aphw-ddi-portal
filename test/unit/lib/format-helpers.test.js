253ba0b9aa7f1ddad20b38a1a2b1ff9fefd1ac10
Chris Cole <whitewaterdesign>
2024-02-29 15:15:24 +0000

    feat: Map OS Places Country Code

---
+++ test/unit/lib/format-helpers.test.js
@@ -0,0 +1,18 @@
+const { mapOsCountryCodeToCountry } = require('../../../app/lib/format-helpers')
+
+describe('format-helpers', () => {
+  describe('mapCountry', () => {
+    test('should map E to England', () => {
+      expect(mapOsCountryCodeToCountry('E')).toBe('England')
+    })
+    test('should map S to Scotland', () => {
+      expect(mapOsCountryCodeToCountry('S')).toBe('Scotland')
+    })
+    test('should map W to Wales', () => {
+      expect(mapOsCountryCodeToCountry('W')).toBe('Wales')
+    })
+    test('should map undefined to undefined', () => {
+      expect(mapOsCountryCodeToCountry(undefined)).toBe(undefined)
+    })
+  })
+})
