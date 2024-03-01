2cbdd711bfce67d56626c7893f39feb4a7b0f961
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-03-01 08:51:22 +0000

    feat: 284781 - Update schema for createCdo - add opt country (#103)
    * feat: 284781 - Update schema for createCdo - add opt country
    
    * build: 284781 - Minor version bump update cdo schema
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- test/mocks/auth.js
+++ test/mocks/auth.js
@@ -5,9 +5,16 @@ const user = {
   username: 'test@example.com'
 }
 
+const userWithDisplayname = {
+  userId: '1',
+  username: 'test@example.com',
+  displayname: 'Example Tester'
+}
+
 const auth = { strategy: 'session-auth', credentials: { scope: [admin], account: { user } } }
 
 module.exports = {
   user,
-  auth
+  auth,
+  userWithDisplayname
 }
