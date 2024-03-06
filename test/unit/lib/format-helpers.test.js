401885b2dbe4de7d193ab71dd85518d7160006c0
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-03-06 11:59:58 +0000

    Bug/284781  bugfixes owner (#111)
    * bug: 284781 - Styling of create cdo Full Summary
    
    * fix: 284781 - Record created separate country
    
    * chore: patch version 0.83.5
    
    * bug: 284781 - PR country styling
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- test/unit/lib/format-helpers.test.js
+++ test/unit/lib/format-helpers.test.js
@@ -1,6 +1,37 @@
-const { mapOsCountryCodeToCountry } = require('../../../app/lib/format-helpers')
+const { mapOsCountryCodeToCountry, formatAddress } = require('../../../app/lib/format-helpers')
 
 describe('format-helpers', () => {
+  describe('formatAddress', () => {
+    const address = {
+      addressLine1: '1 Anywhere St',
+      addressLine2: 'Any Estate',
+      town: 'Chippenham',
+      postcode: 'SN15 4JH',
+      country: 'England'
+    }
+    const expectedWithCountry = ['1 Anywhere St', 'Any Estate', 'Chippenham', 'SN15 4JH', 'England']
+    const expectedWithoutCountry = ['1 Anywhere St', 'Any Estate', 'Chippenham', 'SN15 4JH']
+
+    test('should include country by default', () => {
+      const formattedAddress = formatAddress(address)
+      expect(formattedAddress).toEqual(expectedWithCountry)
+    })
+
+    test('should include country given includeCountry set to true', () => {
+      const formattedAddress = formatAddress(address, false)
+      expect(formattedAddress).toEqual(expectedWithCountry)
+    })
+
+    test('should not include country given includeCountry set to false', () => {
+      const formattedAddress = formatAddress(address, true)
+      expect(formattedAddress).toEqual(expectedWithoutCountry)
+    })
+
+    test('should return null if no address is found', () => {
+      const formattedAddress = formatAddress(undefined)
+      expect(formattedAddress).toEqual(null)
+    })
+  })
   describe('mapCountry', () => {
     test('should map E to England', () => {
       expect(mapOsCountryCodeToCountry('E')).toBe('England')
