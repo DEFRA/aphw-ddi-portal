cb8cd2536c87cfb438b75f1794268c61ae2cdfb5
work <jeremy.barnsley@esynergy.co.uk>
2024-03-21 20:15:28 +0000

    More tests

--- test/integration/narrow/routes/index.test.js
+++ test/integration/narrow/routes/index.test.js
@@ -1,8 +1,20 @@
+const { auth, user } = require('../../../mocks/auth')
+
 describe('Index test', () => {
   const createServer = require('../../../../app/server')
   let server
 
+  jest.mock('../../../../app/auth')
+  const mockAuth = require('../../../../app/auth')
+
+  jest.mock('../../../../app/session/cdo/dog')
+  const { clearAllDogs } = require('../../../../app/session/cdo/dog')
+
+  jest.mock('../../../../app/session/cdo/owner')
+  const { setOwnerDetails } = require('../../../../app/session/cdo/owner')
+
   beforeEach(async () => {
+    mockAuth.getUser.mockReturnValue(user)
     server = await createServer()
     await server.initialize()
   })
@@ -15,6 +27,19 @@ describe('Index test', () => {
 
     const response = await server.inject(options)
     expect(response.statusCode).toBe(302)
+  })
+
+  test('GET / route returns 200 and clears previous details', async () => {
+    const options = {
+      method: 'GET',
+      url: '/',
+      auth
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(200)
+    expect(clearAllDogs).toHaveBeenCalledTimes(1)
+    expect(setOwnerDetails).toHaveBeenCalledTimes(1)
   })
 
   afterEach(async () => {
