21b2d5d714970ee892a1b69bddafea4c7e63fdba
work <jeremy.barnsley@esynergy.co.uk>
2024-03-21 19:58:11 +0000

    Added organisation

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,6 @@
 const { admin } = require('../auth/permissions')
+const { clearAllDogs } = require('../session/cdo/dog')
+const { setOwnerDetails } = require('../session/cdo/owner')
 
 module.exports = {
   method: 'GET',
@@ -6,6 +8,8 @@ module.exports = {
   options: {
     auth: { scope: [admin] },
     handler: (request, h) => {
+      clearAllDogs(request)
+      setOwnerDetails(request, null)
       return h.view('index')
     }
   }
