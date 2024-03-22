5422a970e5b5eb9bb8b04ad754983af16a7ffa1d
Chris Cole <whitewaterdesign>
2024-03-22 14:00:53 +0000

    feat: 343616 - Change name of Create new Record

--- test/integration/narrow/routes/index.test.js
+++ test/integration/narrow/routes/index.test.js
@@ -1,13 +1,20 @@
+const { auth, user } = require('../../../mocks/auth')
+const { JSDOM } = require('jsdom')
+
 describe('Index test', () => {
+  jest.mock('../../../../app/auth')
+  const mockAuth = require('../../../../app/auth')
+
   const createServer = require('../../../../app/server')
   let server
 
   beforeEach(async () => {
+    mockAuth.getUser.mockReturnValue(user)
     server = await createServer()
     await server.initialize()
   })
 
-  test('GET / route returns 302', async () => {
+  test('GET / route returns 302 with no auth', async () => {
     const options = {
       method: 'GET',
       url: '/'
@@ -15,6 +22,19 @@ describe('Index test', () => {
 
     const response = await server.inject(options)
     expect(response.statusCode).toBe(302)
+  })
+
+  test('GET / route returns 200', async () => {
+    const options = {
+      method: 'GET',
+      url: '/',
+      auth
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(200)
+    const { document } = new JSDOM(response.payload).window
+    expect(document.querySelectorAll('.govuk-card--dashboard .govuk-link')[1].textContent).toBe('Process new CDO or Interim Exemption')
   })
 
   afterEach(async () => {
