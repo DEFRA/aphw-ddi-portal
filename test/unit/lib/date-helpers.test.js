65bed8d38903f6d1784af257522d2af325b6799d
work <jeremy.barnsley@esynergy.co.uk>
2024-04-24 13:29:45 +0100

    Added test after review

--- test/unit/lib/date-helpers.test.js
+++ test/unit/lib/date-helpers.test.js
@@ -1,7 +1,7 @@
-const { getElapsed } = require('../../../app/lib/date-helpers')
+const { getElapsed, formatToDateTime } = require('../../../app/lib/date-helpers')
 
 describe('date-helpers', () => {
-  describe('fgetElapsed', () => {
+  describe('getElapsed', () => {
     test('should handle invalid dates', () => {
       const elapsed = getElapsed(null, null)
       expect(elapsed).toEqual('00:00:00')
@@ -20,6 +20,23 @@ describe('date-helpers', () => {
     test('should handle difference when hours/mins/secs in double digits', () => {
       const elapsed = getElapsed('2024-04-01T11:12:13.00000Z', '2024-04-01T01:00:00.00000Z')
       expect(elapsed).toEqual('10:12:13')
+    })
+  })
+
+  describe('formatToDateTime', () => {
+    test('should handle null dates', () => {
+      const result = formatToDateTime(null)
+      expect(result).toBe(null)
+    })
+
+    test('should handle undefined dates', () => {
+      const result = formatToDateTime(undefined)
+      expect(result).toBe(undefined)
+    })
+
+    test('should handle normal dates', () => {
+      const result = formatToDateTime(new Date(2024, 5, 5, 10, 4, 1))
+      expect(result).toBe('05 June 2024 10:04:01')
     })
   })
 })
