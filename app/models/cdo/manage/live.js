0c2e166b3283f667185ea448490011f064caa2af
Chris Cole <whitewaterdesign>
2024-04-25 09:08:16 +0100

    feat: 279908 - Add tabs manage cdos

--- app/models/cdo/manage/live.js
+++ app/models/cdo/manage/live.js
@@ -9,10 +9,30 @@ const { errorPusherDefault } = require('../../../lib/error-helpers')
  * @constructor
  */
 function ViewModel (resultList, tab, sort, backNav, errors) {
+  const tabs = [
+    {
+      visible: true,
+      active: tab === 'live',
+      href: '/cdo/manage',
+      label: 'Live CDOs'
+    },
+    {
+      visible: false,
+      active: tab === 'expired',
+      href: '/cdo/manage/expired',
+      label: 'Expired'
+    },
+    {
+      visible: true,
+      active: tab === 'due',
+      href: '/cdo/manage/due',
+      label: 'Due within 30 days'
+    }
+  ]
   this.model = {
     backLink: backNav.backLink,
     srcHashParam: backNav.srcHashParam,
-    tab,
+    tabs,
     sort: {
       column: 'cdoExpiry',
       order: 'ASC',
--- test/integration/narrow/routes/index.test.js
+++ test/integration/narrow/routes/index.test.js
@@ -38,6 +38,9 @@ describe('Index test', () => {
     expect(response.statusCode).toBe(200)
     const { document } = new JSDOM(response.payload).window
     expect(document.querySelectorAll('.govuk-card--dashboard .govuk-link')[1].textContent).toBe('Process new CDO or Interim Exemption')
+    expect(document.querySelectorAll('.govuk-card--dashboard')[2].textContent.trim()).toContain('Track applications')
+    expect(document.querySelectorAll('.govuk-card--dashboard')[2].textContent.trim()).toContain('Manage CDOs and interim exemptions')
+    expect(document.querySelectorAll('.govuk-card--dashboard')[2].querySelector('.govuk-link').getAttribute('href')).toBe('/cdo/manage')
     expect(clearCdo).toHaveBeenCalledTimes(1)
   })
 
