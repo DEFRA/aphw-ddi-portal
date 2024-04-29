a773c07147defb5d687ce7e50826276ef4172352
Chris Cole <whitewaterdesign>
2024-04-29 10:24:55 +0100

    feat: 362467 - Manage CDO include sort key and order in query schema

--- app/schema/portal/cdo/manage.js
+++ app/schema/portal/cdo/manage.js
@@ -1,7 +1,9 @@
 const Joi = require('joi')
 
 const manageCdosGetschema = Joi.object({
-  tab: Joi.string().valid('live', 'expired', 'due', 'interim').default('live')
+  tab: Joi.string().valid('live', 'expired', 'due', 'interim').default('live'),
+  sortKey: Joi.string().valid('indexNumber', 'cdoExpiry', 'owner', 'policeForce', 'interimExempt').default('cdoExpiry'),
+  sortOrder: Joi.string().valid('ASC', 'DESC').default('ASC')
 })
 
 module.exports = {
--- test/unit/schema/portal/cdo/manage.test.js
+++ test/unit/schema/portal/cdo/manage.test.js
@@ -1,34 +1,111 @@
 const { manageCdosGetschema } = require('../../../../../app/schema/portal/cdo/manage')
 
-describe('Manage CDOs tab navigation', () => {
-  test('should pass validation when tab is undefined', () => {
-    const params = {}
+describe('Manage CDOs', () => {
+  describe('Manage CDOs tab navigation', () => {
+    test('should pass validation when tab is undefined', () => {
+      const params = {}
 
-    const { value } = manageCdosGetschema.validate(params)
+      const { value } = manageCdosGetschema.validate(params)
 
-    expect(value).toMatchObject({
-      tab: 'live'
+      expect(value).toMatchObject({
+        tab: 'live'
+      })
+    })
+
+    test.each(
+      ['live', 'expired', 'due', 'interim']
+    )('should pass validation when tab is %s', (tab) => {
+      const params = { tab }
+
+      const { value, error } = manageCdosGetschema.validate(params)
+
+      expect(value).toMatchObject({ tab })
+      expect(error).toBeUndefined()
+    })
+
+    test('should fail validation with invalid tab', () => {
+      const params = {
+        tab: 'unknown'
+      }
+
+      const { error } = manageCdosGetschema.validate(params)
+
+      expect(error).not.toBeUndefined()
     })
   })
 
-  test.each(
-    ['live', 'expired', 'due', 'interim']
-  )('should pass validation when tab is %s', (tab) => {
-    const params = { tab }
+  describe('Manage CDOs sorting', () => {
+    test.each(
+      [
+        ['indexNumber', 'ASC'],
+        ['indexNumber', 'DESC'],
+        ['interimExempt', 'ASC'],
+        ['interimExempt', 'DESC'],
+        ['cdoExpiry', 'ASC'],
+        ['cdoExpiry', 'DESC'],
+        ['owner', 'ASC'],
+        ['owner', 'DESC'],
+        ['policeForce', 'ASC'],
+        ['policeForce', 'DESC']
+      ]
+    )('should pass validation when sort key is %s and order is %s', (sortKey, sortOrder) => {
+      const params = { sortKey, sortOrder }
 
-    const { value, error } = manageCdosGetschema.validate(params)
+      const { value, error } = manageCdosGetschema.validate(params)
 
-    expect(value).toMatchObject({ tab })
-    expect(error).toBeUndefined()
-  })
+      expect(value).toMatchObject({
+        sortKey,
+        sortOrder
+      })
+      expect(error).toBeUndefined()
+    })
 
-  test('should fail validation with invalid tab', () => {
-    const params = {
-      tab: 'unknown'
-    }
+    test.each(
+      [
+        ['indexNumber'],
+        ['interimExempt'],
+        ['cdoExpiry'],
+        ['owner'],
+        ['policeForce']
+      ]
+    )('should pass validation when sort key is %s and order is undefined', (sortKey) => {
+      const params = { sortKey }
 
-    const { error } = manageCdosGetschema.validate(params)
+      const { value, error } = manageCdosGetschema.validate(params)
 
-    expect(error).not.toBeUndefined()
+      expect(value).toMatchObject({
+        sortKey,
+        sortOrder: 'ASC'
+      })
+      expect(error).toBeUndefined()
+    })
+
+    test('should default to cdoExpiry, ASC', () => {
+      const params = {}
+
+      const { value, error } = manageCdosGetschema.validate(params)
+
+      expect(value).toMatchObject({
+        sortKey: 'cdoExpiry',
+        sortOrder: 'ASC'
+      })
+      expect(error).toBeUndefined()
+    })
+
+    test('should not validate if sort key is invalid', () => {
+      const params = { sortKey: 'unknown' }
+
+      const { error } = manageCdosGetschema.validate(params)
+
+      expect(error).not.toBeUndefined()
+    })
+
+    test('should not validate if sort order is invalid', () => {
+      const params = { sortKey: 'owner', sortOrder: 'ASCENDING' }
+
+      const { error } = manageCdosGetschema.validate(params)
+
+      expect(error).not.toBeUndefined()
+    })
   })
 })
