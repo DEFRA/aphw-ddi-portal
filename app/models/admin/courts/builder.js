168016574a3392ac46d011b4249f01ddd258e69d
Chris Cole <whitewaterdesign>
2024-05-02 19:42:16 +0100

    feat: 371208 - CourtRemovedViewModel

--- app/models/admin/courts/builder.js
+++ app/models/admin/courts/builder.js
@@ -1,6 +1,17 @@
 const { routes } = require('../../../constants/admin')
 const ViewModel = require('../success')
 
+const breadcrumbs = [
+  {
+    label: 'Home',
+    link: '/'
+  },
+  {
+    label: 'Admin',
+    link: routes.index.get
+  }
+]
+
 /**
  *
  * @param court
@@ -8,18 +19,24 @@ const ViewModel = require('../success')
  */
 const CourtAddedViewModel = (court) => {
   return new ViewModel({
-    breadcrumbs: [
-      {
-        label: 'Home',
-        link: '/'
-      },
-      {
-        label: 'Admin',
-        link: routes.index.get
-      }
-    ],
+    breadcrumbs,
     titleHtml: `You added<br /> ${court}`,
-    bodyContent: `${court} is available in the Index.`,
+    bodyContent: [`${court} is available in the Index.`],
+    bottomLink: {
+      link: routes.courts.get,
+      label: 'Add or remove a court'
+    }
+  })
+}
+
+const CourtRemovedViewModel = (court) => {
+  return new ViewModel({
+    breadcrumbs,
+    titleHtml: `You removed<br /> ${court}`,
+    bodyContent: [
+      `${court} is removed from the Index and will not be available for new applications.`,
+      'Existing records are unchanged.'
+    ],
     bottomLink: {
       link: routes.courts.get,
       label: 'Add or remove a court'
@@ -28,5 +45,6 @@ const CourtAddedViewModel = (court) => {
 }
 
 module.exports = {
-  CourtAddedViewModel
+  CourtAddedViewModel,
+  CourtRemovedViewModel
 }
