2ec05e500664f88c3f35465170520ac5fefc7021
work <jeremy.barnsley@esynergy.co.uk>
2024-05-16 15:47:58 +0100

    Lint fix

--- test/integration/local/storage/repos/download-blob.test.js
+++ test/integration/local/storage/repos/download-blob.test.js
@@ -5,7 +5,7 @@ const { uploadRegisterFile } = require('../../../../../app/storage/repos/registe
 
 const streamToBuffer = async (readableStream) => {
   return new Promise((resolve, reject) => {
-    const chunks = [];
+    const chunks = []
     readableStream.on('data', (data) => {
       chunks.push(data instanceof Buffer ? data : Buffer.from(data))
     })
