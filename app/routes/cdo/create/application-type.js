beeb6563e73a6ded36d3c21c76ac48365b286cc5
work <jeremy.barnsley@esynergy.co.uk>
2024-05-23 11:28:38 +0100

    Fixed back nav

--- app/routes/cdo/create/application-type.js
+++ app/routes/cdo/create/application-type.js
@@ -3,9 +3,14 @@ const { routes: ownerRoutes } = require('../../../constants/cdo/owner')
 const { anyLoggedInUser } = require('../../../auth/permissions')
 const { addDateComponents, removeDateComponents } = require('../../../lib/date-helpers')
 const ViewModel = require('../../../models/cdo/create/application-type')
-const { getDog, setDog } = require('../../../session/cdo/dog')
+const { getDog, setDog, getMicrochipResults } = require('../../../session/cdo/dog')
 const { setPoliceForce } = require('../../../lib/model-helpers')
 const { validatePayload } = require('../../../schema/portal/cdo/application-type')
+
+const determineBackLink = request => {
+  const existingDogs = getMicrochipResults(request)
+  return existingDogs?.results?.length > 0 ? dogRoutes.microchipResults.get : dogRoutes.selectExistingDog.get
+}
 
 module.exports = [
   {
@@ -34,7 +39,7 @@ module.exports = [
           addDateComponents(dog, keys.cdoIssued)
         }
 
-        return h.view(views.applicationType, new ViewModel(dog))
+        return h.view(views.applicationType, new ViewModel(dog, determineBackLink(request)))
       }
     }
   },
@@ -48,7 +53,7 @@ module.exports = [
         failAction: async (request, h, error) => {
           const dog = request.payload
 
-          return h.view(views.applicationType, new ViewModel(dog, error)).code(400).takeover()
+          return h.view(views.applicationType, new ViewModel(dog, determineBackLink(request), error)).code(400).takeover()
         }
       },
       handler: async (request, h) => {
