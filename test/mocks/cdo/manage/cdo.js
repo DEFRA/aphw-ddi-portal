c5f6644373b717a75190ea26e227bbaee24552f1
work <jeremy.barnsley@esynergy.co.uk>
2024-06-26 15:07:31 +0100

    Corrected mock data Tweaked memory allocation for tests

--- test/mocks/cdo/manage/cdo.js
+++ test/mocks/cdo/manage/cdo.js
@@ -4,49 +4,49 @@ const noTasksStartedYet = {
       key: 'applicationPackSent',
       available: true,
       completed: false,
-      editable: true,
+      readonly: false,
       timestamp: undefined
     },
     insuranceDetailsRecorded: {
       key: 'insuranceDetailsRecorded',
       available: false,
       completed: false,
-      editable: true,
+      readonly: false,
       timestamp: undefined
     },
     microchipNumberRecorded: {
       key: 'microchipNumberRecorded',
       available: false,
       completed: false,
-      editable: true,
+      readonly: false,
       timestamp: undefined
     },
     applicationFeePaid: {
       key: 'applicationFeePaid',
       available: false,
       completed: false,
-      editable: true,
+      readonly: false,
       timestamp: undefined
     },
     form2Sent: {
       key: 'form2Sent',
       available: false,
       completed: false,
-      editable: true,
+      readonly: false,
       timestamp: undefined
     },
     verificationDateRecorded: {
       key: 'verificationDateRecorded',
       available: false,
       completed: false,
-      editable: true,
+      readonly: false,
       timestamp: undefined
     },
     certificateIssued: {
       key: 'certificateIssued',
       available: false,
       completed: false,
-      editable: true,
+      readonly: true,
       timestamp: undefined
     }
   }
