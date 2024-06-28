44f2597c490264e24a90dc9fed4f94d7b3e7152c
Chris Cole <whitewaterdesign>
2024-06-28 08:14:31 +0100

    test: add contract tests again

--- app/routes/cdo/manage/tasks/generic-task.js
+++ app/routes/cdo/manage/tasks/generic-task.js
@@ -19,6 +19,7 @@ module.exports = [{
       const dogIndex = request.params.dogIndex
 
       const data = await getTaskData(dogIndex, taskName)
+      console.log('~~~~~~ Chris Debug ~~~~~~ ', 'Data', data)
 
       const backNav = addBackNavigation(request)
 
