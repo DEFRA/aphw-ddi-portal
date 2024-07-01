ec3a6479c0d6c42bc0f6a77caaf73f2b9c18fff1
Chris Cole <whitewaterdesign>
2024-07-01 10:32:26 +0100

    chore: More logging on cookies

--- app/cookies.js
+++ app/cookies.js
@@ -1,33 +1,47 @@
 const config = require('./config').cookieOptions
 
 const getCurrentPolicy = (request, h) => {
+  console.log('~~~~~~ Chris Debug ~~~~~~ getCurrentPolicy', '')
   let cookiesPolicy = request.state.cookies_policy
   if (!cookiesPolicy) {
+    console.log('~~~~~~ Chris Debug ~~~~~~ getCurrentPolicy - creating default policy', '')
     cookiesPolicy = createDefaultPolicy(h)
+    console.log('~~~~~~ Chris Debug ~~~~~~ getCurrentPolicy - created default policy', '')
   }
   return cookiesPolicy
 }
 
 const createDefaultPolicy = (h) => {
+  console.log('~~~~~~ Chris Debug ~~~~~~ createDefaultPolicy', '')
   const cookiesPolicy = { confirmed: false, essential: true, analytics: false }
   h.state('cookies_policy', cookiesPolicy, config)
+  console.log('~~~~~~ Chris Debug ~~~~~~ createDefaultPolicy - success', '')
+
   return cookiesPolicy
 }
 
 const updatePolicy = (request, h, analytics) => {
+  console.log('~~~~~~ Chris Debug ~~~~~~ updatePolicy', '')
   let cookiesPolicy = request.state.cookies_policy
   if (!cookiesPolicy) {
+    console.log('~~~~~~ Chris Debug ~~~~~~ updatePolicy - creating default policy', '')
     cookiesPolicy = createDefaultPolicy(h)
   }
+
+  console.log('~~~~~~ Chris Debug ~~~~~~ updatePolicy - created default policy', '')
 
   cookiesPolicy.analytics = analytics
   cookiesPolicy.confirmed = true
 
+  console.log('~~~~~~ Chris Debug ~~~~~~ updatePolicy - setting policy state', 'CookiesPolicy', cookiesPolicy)
   h.state('cookies_policy', cookiesPolicy, config)
+  console.log('~~~~~~ Chris Debug ~~~~~~ updatePolicy - cookies_policy', '')
 
   if (!analytics) {
+    console.log('~~~~~~ Chris Debug ~~~~~~ updatePolicy - unsetting _ga _gid', '')
     h.unstate('_ga')
     h.unstate('_gid')
+    console.log('~~~~~~ Chris Debug ~~~~~~ updatePolicy - success unset', '')
   }
 }
 
