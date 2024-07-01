231d37c1d34048a81e61fd99e8a3b5886f20ba01
work <jeremy.barnsley@esynergy.co.uk>
2024-07-01 14:04:20 +0100

    Tidied debug

--- app/plugins/cookies.js
+++ app/plugins/cookies.js
@@ -10,17 +10,13 @@ module.exports = {
       console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies cookies_policy set', '')
 
       server.ext('onPreResponse', (request, h) => {
-        console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies onPreResponse', '')
         const statusCode = request.response.statusCode
-        console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies onPreResponse status', statusCode)
-        console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies onPreResponse variety', request.response.variety)
         if (request.response.variety === 'view' && statusCode !== 404 && statusCode !== 500 && request.response.source.manager._context) {
-          console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies getting cookiesPolicy', '')
+          console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies getting cookiesPolicy', request.response.source.manager._context)
           const cookiesPolicy = getCurrentPolicy(request, h)
           console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies success getting cookiesPolicy', cookiesPolicy)
           request.response.source.manager._context.cookiesPolicy = cookiesPolicy
         }
-        console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies returning', '')
         return h.continue
       })
     }
