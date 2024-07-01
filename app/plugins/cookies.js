0f8588be4b40b7099f8bfdc5ae62f74f83449581
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2024-07-01 12:21:45 +0100

    Extra cookie debug (#231)
    * Extra cookie debug
    Bumped cookie version
    
    * Patch bump
    
    * Extra debug
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>
    Co-authored-by: Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>

--- app/plugins/cookies.js
+++ app/plugins/cookies.js
@@ -12,10 +12,12 @@ module.exports = {
       server.ext('onPreResponse', (request, h) => {
         console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies onPreResponse', '')
         const statusCode = request.response.statusCode
+        console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies onPreResponse status', statusCode)
+        console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies onPreResponse variety', request.response.variety)
         if (request.response.variety === 'view' && statusCode !== 404 && statusCode !== 500 && request.response.source.manager._context) {
           console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies getting cookiesPolicy', '')
           const cookiesPolicy = getCurrentPolicy(request, h)
-          console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies success getting cookiesPolicy', '')
+          console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies success getting cookiesPolicy', cookiesPolicy)
           request.response.source.manager._context.cookiesPolicy = cookiesPolicy
         }
         console.log('~~~~~~ Chris Debug ~~~~~~ plugins/cookies returning', '')
