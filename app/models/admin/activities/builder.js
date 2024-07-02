bb07ec3b79c1a73f481771331c37fe11745a76cb
work <jeremy.barnsley@esynergy.co.uk>
2024-07-02 17:39:55 +0100

    Extra test

--- app/models/admin/activities/builder.js
+++ app/models/admin/activities/builder.js
@@ -74,5 +74,6 @@ const ActivityRemovedViewModel = (activity) => {
 module.exports = {
   ActivityListViewModel,
   ActivityAddedViewModel,
-  ActivityRemovedViewModel
+  ActivityRemovedViewModel,
+  disableIfNeeded
 }
--- test/unit/models/admin/activities/builder.test.js
+++ test/unit/models/admin/activities/builder.test.js
@@ -1,4 +1,4 @@
-const { ActivityAddedViewModel } = require('../../../../../app/models/admin/activities/builder')
+const { ActivityAddedViewModel, disableIfNeeded } = require('../../../../../app/models/admin/activities/builder')
 describe('builder', () => {
   describe('ActivityAddedViewModel', () => {
     test('should create receive models', () => {
@@ -8,6 +8,19 @@ describe('builder', () => {
         activityType: 'receive'
       })
       expect(activityAddedViewModel.model.bodyContent).toEqual(['Judicial review notice is available in the Dog record receive activities.'])
+    })
+
+    test('should disable as appropriate', () => {
+      const rows = [
+        { id: 1, label: 'Label 1' },
+        { id: 2, label: 'Application pack' },
+        { id: 3, label: 'Label 3' }
+      ]
+      const res = disableIfNeeded(rows)
+      expect(res.length).toBe(3)
+      expect(res[0].canRemove).toBeTruthy()
+      expect(res[1].canRemove).toBeFalsy()
+      expect(res[2].canRemove).toBeTruthy()
     })
   })
 })
