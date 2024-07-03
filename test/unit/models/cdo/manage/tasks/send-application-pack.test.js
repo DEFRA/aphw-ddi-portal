747dbd4eecd8674d6e81c3572cd36518583253ef
work <jeremy.barnsley@esynergy.co.uk>
2024-07-03 15:40:55 +0100

    Added model tests to cover ternary

---
+++ test/unit/models/cdo/manage/tasks/send-application-pack.test.js
@@ -0,0 +1,17 @@
+const ViewModel = require('../../../../../../app/models/cdo/manage/tasks/send-application-pack')
+
+describe('SendApplicationPack Model', () => {
+  test('should handle backnav', () => {
+    const backNav = { backLink: '/some-non-root-url' }
+    const data = { indexNumber: 'ED12345', task: { completed: false } }
+    const res = new ViewModel(data, backNav)
+    expect(res.model.backLink).toBe('/some-non-root-url')
+  })
+
+  test('should handle root/missing backnav', () => {
+    const backNav = { backLink: '/' }
+    const data = { indexNumber: 'ED12345', task: { completed: false } }
+    const res = new ViewModel(data, backNav)
+    expect(res.model.backLink).toBe('/cdo/manage/cdo/ED12345')
+  })
+})
