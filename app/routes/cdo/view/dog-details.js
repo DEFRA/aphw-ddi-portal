3f2c67c028ce2a754555b8a1f1f833e8e4e3b420
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2024-07-11 10:58:09 +0100

    Redirects to Manage CDO as appropriate (#248)
    * Redirects to Manage CDO as appropriate
    
    * Patch bump
    
    ---------
    
    Co-authored-by: work <jeremy.barnsley@esynergy.co.uk>

--- app/routes/cdo/view/dog-details.js
+++ app/routes/cdo/view/dog-details.js
@@ -1,4 +1,6 @@
 const { routes, views } = require('../../../constants/cdo/dog')
+const { routes: cdoRoutes } = require('../../../constants/cdo/index')
+const statuses = require('../../../constants/cdo/status')
 const { anyLoggedInUser } = require('../../../auth/permissions')
 const ViewModel = require('../../../models/cdo/view/dog-details')
 const { getCdo } = require('../../../api/ddi-index-api/cdo')
@@ -12,13 +14,19 @@ module.exports = [
     options: {
       auth: { scope: anyLoggedInUser },
       handler: async (request, h) => {
-        const cdo = await getCdo(request.params.indexNumber)
+        const indexNumber = request.params.indexNumber
+        const cdo = await getCdo(indexNumber)
 
         if (cdo === undefined) {
           return h.response().code(404).takeover()
         }
 
         setActivityDetails(request, null)
+
+        if (cdo.dog.status === statuses.PreExempt && request.query.force !== 'true') {
+          console.log('redirect****** status', cdo.dog.status)
+          return h.redirect(`${cdoRoutes.manageCdo.get}/${indexNumber}?src=${request.query.src}`)
+        }
 
         const backNav = addBackNavigation(request, true)
 
