0cf16774a0c3eb7a75dda29c3a10451ec972e943
Chris Cole <whitewaterdesign>
2024-07-17 09:39:44 +0100

    doc: 408716 - Setup jsdoc types for GovukCheckbox

---
+++ app/models/builders/builders.js
@@ -0,0 +1,13 @@
+/**
+ * @param {import('./components').GovukCheckBox} govukCheckbox
+ * @return {import('./components').GovukCheckBox}
+ */
+const govukCheckbox = (govukCheckbox) => govukCheckbox
+
+/**
+ * @param {import('./components').ItemsArrayObject} govukCheckboxItem
+ * @return {import('./components').ItemsArrayObject}
+ */
+const govukCheckboxItem = (govukCheckboxItem) => govukCheckboxItem
+
+module.exports = { govukCheckbox, govukCheckboxItem }
