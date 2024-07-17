a1f92e6e6b289138cf0427504213bedb9307d5c8
Chris Cole <whitewaterdesign>
2024-07-17 11:33:37 +0100

    feat: 408716 - Map getBreachCategories labels to Sentence Case

--- app/api/ddi-index-api/dog-breaches.js
+++ app/api/ddi-index-api/dog-breaches.js
@@ -6,10 +6,29 @@ const options = {
   json: true
 }
 
+/**
+ * @typedef BreachCategory
+ * @property {number} id
+ * @property {string} label
+ * @property {string} short_name
+ */
+
+/**
+ * @return {Promise<BreachCategory[]>}
+ */
 const getBreachCategories = async () => {
   const payload = await get(`${dogBreachesEndpoint}/categories`, options)
 
-  return payload.breachCategories
+  const breachCategories = payload.breachCategories.map(breachCategory => {
+    const [head, ...tail] = breachCategory.label
+    const label = head.toUpperCase() + tail.join('')
+
+    return {
+      ...breachCategory,
+      label
+    }
+  })
+  return breachCategories
 }
 
 module.exports = {
--- test/unit/api/ddi-index-api/dog-breaches.test.js
+++ test/unit/api/ddi-index-api/dog-breaches.test.js
@@ -12,6 +12,24 @@ describe('DDI API Dog Breeches', () => {
     const mockBreachCategories = [
       {
         id: 1,
+        label: 'dog not covered by third party insurance',
+        short_name: 'NOT_COVERED_BY_INSURANCE'
+      },
+      {
+        id: 2,
+        label: 'dog not kept on lead or muzzled',
+        short_name: 'NOT_ON_LEAD_OR_MUZZLED'
+      },
+      {
+        id: 3,
+        label: 'dog kept in insecure place',
+        short_name: 'INSECURE_PLACE'
+      }
+    ]
+
+    const expectdBreachCategories = [
+      {
+        id: 1,
         label: 'Dog not covered by third party insurance',
         short_name: 'NOT_COVERED_BY_INSURANCE'
       },
@@ -33,6 +51,6 @@ describe('DDI API Dog Breeches', () => {
     const breachCategories = await getBreachCategories()
     expect(breachCategories).toBeInstanceOf(Array)
     expect(breachCategories).toHaveLength(3)
-    expect(breachCategories).toEqual(mockBreachCategories)
+    expect(breachCategories).toEqual(expectdBreachCategories)
   })
 })
