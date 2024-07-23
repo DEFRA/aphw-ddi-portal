4ec5cafcd1015984b43bb6350a6006b4e4e646b6
Chris Cole <56303993+whitewaterdesign@users.noreply.github.com>
2024-07-23 17:28:27 +0100

    Feat/414064  testing endpoint for neutering (#268)
    * feat: 414064 - Api method for neuteringDeadline calls
    
    * feat: 414064 - Testing endpoint for neutering
    
    * chore: patch v0.94.82
    
    * chore: cleanup dummy commit
    
    ---------
    
    Co-authored-by: Chris Cole <whitewaterdesign>

--- test/unit/api/ddi-index-api/jobs.test.js
+++ test/unit/api/ddi-index-api/jobs.test.js
@@ -1,4 +1,4 @@
-const { purgeSoftDelete } = require('../../../../app/api/ddi-index-api/jobs')
+const { purgeSoftDelete, neuteringDeadline } = require('../../../../app/api/ddi-index-api/jobs')
 const { post } = require('../../../../app/api/ddi-index-api/base')
 jest.mock('../../../../app/api/ddi-index-api/base')
 
@@ -17,5 +17,17 @@ describe('Jobs tests', () => {
     post.mockResolvedValue({ payload: {} })
     await purgeSoftDelete('2010-06-20')
     expect(post).toHaveBeenCalledWith('jobs/purge-soft-delete?today=2010-06-20')
+  })
+
+  test('neuteringDeadline calls endpoint', async () => {
+    post.mockResolvedValue({ payload: {} })
+    await neuteringDeadline()
+    expect(post).toHaveBeenCalledWith('jobs/neutering-deadline')
+  })
+
+  test('neuteringDeadline calls endpoint with param', async () => {
+    post.mockResolvedValue({ payload: {} })
+    await neuteringDeadline('2010-06-20')
+    expect(post).toHaveBeenCalledWith('jobs/neutering-deadline?today=2010-06-20')
   })
 })
