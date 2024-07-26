40d4a73ffa48799931849d4884b984535938f2ee
work <jeremy.barnsley@esynergy.co.uk>
2024-07-26 13:41:56 +0100

    Removed test

--- test/unit/plugins/view-context.test.js
+++
@@ -1,8 +0,0 @@
-const { logError } = require('../../../app/plugins/view-context')
-
-describe('ViewContext plugin', () => {
-  test('should throw', () => {
-    const err = new Error('view-context-error')
-    expect(() => logError(err)).toThrow(err)
-  })
-})
