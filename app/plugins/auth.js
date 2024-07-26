32f7557589b6be01c9713040ab41657aa4511565
work <jeremy.barnsley@esynergy.co.uk>
2024-07-26 10:37:21 +0100

    Extra debug

--- app/plugins/auth.js
+++ app/plugins/auth.js
@@ -24,6 +24,7 @@ module.exports = {
       server.auth.default('session-auth')
 
       server.ext('onPreAuth', async (request, h) => {
+        console.log(`plugins/auth request.path ${request.path}`)
         if (request.auth.credentials) {
           await auth.refresh(request.auth.credentials.account, request.cookieAuth)
         }
