fead4da0d281d595e21cd82857ce989b6a4654d5
work <jeremy.barnsley@esynergy.co.uk>
2024-07-30 17:36:28 +0100

    Temp

--- app/schema/portal/search/basic.js
+++ app/schema/portal/search/basic.js
@@ -1,10 +1,10 @@
 const Joi = require('joi')
 
-const validCharsRegex = /^[a-zA-Z0-9-_\s]+$/
+const validCharsRegex = /^[a-zA-Z0-9-.@_\s]+$/
 const schema = Joi.object({
   searchTerms: Joi.string().trim().required().regex(validCharsRegex).max(100).messages({
     'string.empty': 'Enter search term(s)',
-    'string.pattern.base': 'No symbols allowed other than spaces and hypens'
+    'string.pattern.base': 'Only the following symbols are allowed . @ - _'
   }),
   searchType: Joi.string().required().messages({
     '*': 'Select a search type'
