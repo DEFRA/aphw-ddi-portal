602a91a2927ea252a7ef0079fc89a09ebe99be65
work <jeremy.barnsley@esynergy.co.uk>
2024-08-01 10:26:51 +0100

    Removed debug

--- app/api/ddi-index-api/activities.js
+++ app/api/ddi-index-api/activities.js
@@ -35,11 +35,9 @@ const getActivityById = async (activityId) => {
 
 const recordActivity = async (activity, user) => {
   const activityEntity = await getActivityById(activity.activity)
-  console.log('activityEntity', activityEntity)
   activity.targetPk = activityEntity.activity_event?.target_primary_key
 
   if (activity.targetPk !== activity.source && activity.targetPk === 'owner') {
-    console.log('owner', await getDogOwner(activity.pk))
     activity.pk = (await getDogOwner(activity.pk)).personReference
   }
 
