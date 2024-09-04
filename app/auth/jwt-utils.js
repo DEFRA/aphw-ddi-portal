0f49678e6b269b3b4358478c1bac2940f7101f37
Chris Cole <whitewaterdesign>
2024-09-04 20:21:50 +0100

    feat: 430523 - createBearer auth method

--- app/auth/jwt-utils.js
+++ app/auth/jwt-utils.js
@@ -1,15 +1,44 @@
 const jwt = require('jsonwebtoken')
 const config = require('../config')
-const generateToken = (payload) => {
+
+const generateToken = (payload, { audience, issuer }) => {
   const secretKey = config.authConfig.secret
   const options = {
-    expiresIn: '1h'
+    expiresIn: '1h',
+    algorithm: 'HS256',
+    audience,
+    issuer
   }
 
-  const token = jwt.sign(payload, secretKey, options)
-  return token
+  return jwt.sign(payload, secretKey, options)
+}
+
+const createJwtToken = (audience) => (username, displayname, scopes) => {
+  const options = {
+    audience,
+    issuer: 'aphw-ddi-portal'
+  }
+
+  return generateToken({
+    scopes,
+    username,
+    displayname
+  }, options)
+}
+
+const createBearerHeader = (audience) => (user, request) => {
+  const username = user?.username
+  const displayname = user?.displayname
+  const scopes = request.auth?.credentials?.scope
+  const token = createJwtToken(audience)(username, displayname, scopes)
+
+  return {
+    Authorization: `Bearer ${token}`
+  }
 }
 
 module.exports = {
-  generateToken
+  generateToken,
+  createJwtToken,
+  createBearerHeader
 }
--- test/unit/auth/jwt-utils.test.js
+++ test/unit/auth/jwt-utils.test.js
@@ -1,24 +1,100 @@
 const jwt = require('jsonwebtoken')
+
 describe('jwt-utils', () => {
+  jest.mock('../../../app/lib/environment-helpers')
+  const { getEnvironmentVariable } = require('../../../app/lib/environment-helpers')
+  getEnvironmentVariable.mockImplementation((envVar) => {
+    if (envVar === 'PRIVATE_KEY') {
+      return 'abcdedfgh'
+    }
+
+    if (envVar === 'DDI_API_BASE_URL') {
+      return 'https://example.abc'
+    }
+
+    return process.env[envVar]
+  })
+
+  const { generateToken, createJwtToken, createBearerHeader } = require('../../../app/auth/jwt-utils')
+
   describe('generateToken', () => {
-    jest.mock('../../../app/lib/environment-helpers')
-    const { getEnvironmentVariable } = require('../../../app/lib/environment-helpers')
-    getEnvironmentVariable.mockImplementation((envVar) => {
-      if (envVar === 'PRIVATE_KEY') {
-        return 'abcdedfgh'
-      }
-      return process.env[envVar]
-    })
-
-    const { generateToken } = require('../../../app/auth/jwt-utils')
-
     test('should generate a token', () => {
-      const token = generateToken({ username: 'bob@builder.com' })
+      const token = generateToken({ username: 'bob@builder.com' }, { audience: 'https://example.abc', issuer: 'abc' })
+      expect(typeof token).toBe('string')
       expect(jwt.verify(token, 'abcdedfgh')).toEqual({
         username: 'bob@builder.com',
         exp: expect.any(Number),
-        iat: expect.any(Number)
+        iat: expect.any(Number),
+        iss: 'abc',
+        aud: 'https://example.abc'
       })
+    })
+  })
+
+  describe('createJwtToken', () => {
+    test('should generate an azure ad token', () => {
+      const expected = {
+        exp: expect.any(Number),
+        iat: expect.any(Number),
+        iss: 'aphw-ddi-portal',
+        scopes: ['abc'],
+        username: 'bob@builder.com',
+        displayname: 'Bob the Builder',
+        aud: 'https://example.abc'
+      }
+
+      const token = createJwtToken('https://example.abc')('bob@builder.com', 'Bob the Builder', ['abc'])
+
+      const decodedToken = jwt.verify(
+        token,
+        'abcdedfgh',
+        {
+          audience: 'https://example.abc',
+          algorithms: ['HS256'],
+          issuer: 'aphw-ddi-portal'
+        })
+      expect(typeof token).toBe('string')
+      expect(decodedToken).toEqual(expected)
+    })
+  })
+
+  describe('createBearer', () => {
+    test('should create a Bearer Header', () => {
+      const expected = {
+        exp: expect.any(Number),
+        iat: expect.any(Number),
+        iss: 'aphw-ddi-portal',
+        scopes: ['Dog.Index.Standard'],
+        username: 'bob@builder.com',
+        displayname: 'Bob the Builder',
+        aud: 'https://example.abc'
+      }
+      const request = {
+        auth: {
+          credentials: {
+            scope: ['Dog.Index.Standard']
+          }
+        }
+      }
+
+      const user = {
+        username: 'bob@builder.com',
+        displayname: 'Bob the Builder'
+      }
+      const { Authorization } = createBearerHeader('https://example.abc')(user, request)
+
+      const token = Authorization.replace('Bearer ', '')
+
+      const decodedToken = jwt.verify(
+        token,
+        'abcdedfgh',
+        {
+          audience: 'https://example.abc',
+          algorithms: ['HS256'],
+          issuer: 'aphw-ddi-portal'
+        })
+
+      expect(decodedToken).toEqual(expected)
     })
   })
 })
