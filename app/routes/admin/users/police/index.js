d66588143d33cec5736a1a8be6a9325091a74e39
Chris Cole <whitewaterdesign>
2024-10-04 13:50:14 +0100

    feat: 439716 - Add POST /admin/users/police

--- app/routes/admin/users/police/index.js
+++ app/routes/admin/users/police/index.js
@@ -1,6 +1,7 @@
 const { routes, views } = require('../../../../constants/admin')
 const { admin } = require('../../../../auth/permissions')
 const ViewModel = require('../../../../models/common/add-or-remove')
+const { validatePayload } = require('../../../../schema/portal/common/do-you-want')
 
 module.exports = [
   {
@@ -12,6 +13,33 @@ module.exports = [
         return h.view(views.addOrRemove, new ViewModel({
           optionText: 'Do you want to add or remove police officers?'
         }))
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: `${routes.policeUsers.post}`,
+    options: {
+      auth: { scope: [admin] },
+      validate: {
+        payload: validatePayload,
+        failAction: async (request, h, error) => {
+          return h.view(views.addOrRemove, new ViewModel({
+            optionText: 'Do you want to add or remove police officers?'
+          }, undefined, error)).code(400).takeover()
+        }
+      },
+      handler: async (request, h) => {
+        const addRemovePoliceUser = request.payload.addOrRemove
+        let redirectUrl
+
+        if (addRemovePoliceUser === 'remove') {
+          redirectUrl = routes.removePoliceUser.get
+        } else {
+          redirectUrl = routes.addPoliceUser.get
+        }
+
+        return h.redirect(redirectUrl)
       }
     }
   }
--- test/integration/narrow/routes/admin/users/police/index.test.js
+++ test/integration/narrow/routes/admin/users/police/index.test.js
@@ -51,5 +51,66 @@ describe('Police users page', () => {
 
       expect(response.statusCode).toBe(403)
     })
+
+    test('POST /admin/users/police add police user route returns 200', async () => {
+      const options = {
+        method: 'POST',
+        url: '/admin/users/police',
+        auth,
+        payload: {
+          addOrRemove: 'add'
+        }
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(302)
+      expect(response.headers.location).toBe('/admin/users/police/add')
+    })
+
+    test('POST /admin/users/police remove police user route returns 200', async () => {
+      const options = {
+        method: 'POST',
+        url: '/admin/users/police',
+        auth,
+        payload: {
+          addOrRemove: 'remove'
+        }
+      }
+
+      const response = await server.inject(options)
+
+      expect(response.statusCode).toBe(302)
+      expect(response.headers.location).toBe('/admin/users/police/remove')
+    })
+
+    test('POST /admin/users/police without an option returns 400', async () => {
+      const options = {
+        method: 'POST',
+        url: '/admin/users/police',
+        auth,
+        payload: {}
+      }
+
+      const response = await server.inject(options)
+
+      const { document } = new JSDOM(response.payload).window
+      expect(document.querySelector('.govuk-fieldset__heading').textContent.trim()).toBe('Do you want to add or remove police officers?')
+      expect(response.statusCode).toBe(400)
+    })
+
+    test('POST /admin/users/police route returns 302 if not auth', async () => {
+      const fd = new FormData()
+
+      const options = {
+        method: 'POST',
+        url: '/admin/users/police',
+        headers: fd.getHeaders(),
+        payload: fd.getBuffer()
+      }
+
+      const response = await server.inject(options)
+      expect(response.statusCode).toBe(302)
+    })
   })
 })
