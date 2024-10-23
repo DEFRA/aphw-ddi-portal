1caffd0904f93734ef33f82c192f3240372dd2aa
work <jeremy.barnsley@esynergy.co.uk>
2024-10-23 11:03:27 +0100

    Wording change

--- test/integration/narrow/routes/cdo/edit/police-force-changed.test.js
+++ test/integration/narrow/routes/cdo/edit/police-force-changed.test.js
@@ -19,7 +19,7 @@ describe('Police force changed', () => {
   })
 
   describe('GET /cdo/edit/police-force-changed', () => {
-    test('route returns 200', async () => {
+    test('route returns 200 - single dog', async () => {
       getFromSession.mockReturnValue({
         changed: true,
         policeForceName: 'New force',
@@ -35,6 +35,27 @@ describe('Police force changed', () => {
       const response = await server.inject(options)
       const { document } = (new JSDOM(response.payload)).window
       expect(document.querySelector('h1').textContent.trim()).toBe('The police force for the dog record has changed')
+      expect(document.querySelector('form p.govuk-body').textContent.trim()).toBe('The police force is now New force.')
+
+      expect(response.statusCode).toBe(200)
+    })
+
+    test('route returns 200 - multiple dogs', async () => {
+      getFromSession.mockReturnValue({
+        changed: true,
+        policeForceName: 'New force',
+        numOfDogs: 2
+      })
+
+      const options = {
+        method: 'GET',
+        url: '/cdo/edit/police-force-changed',
+        auth
+      }
+
+      const response = await server.inject(options)
+      const { document } = (new JSDOM(response.payload)).window
+      expect(document.querySelector('h1').textContent.trim()).toBe('The police force for the dog records has changed')
       expect(document.querySelector('form p.govuk-body').textContent.trim()).toBe('The police force is now New force.')
 
       expect(response.statusCode).toBe(200)
