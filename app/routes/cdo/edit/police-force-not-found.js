d52f54c4958353f89e31eb7d8675e02bb70ab19f
work <jeremy.barnsley@esynergy.co.uk>
2024-11-01 10:36:02 +0000

    Removed debug

--- app/routes/cdo/edit/police-force-not-found.js
+++ app/routes/cdo/edit/police-force-not-found.js
@@ -25,9 +25,7 @@ module.exports = [
           return h.response().code(404).takeover()
         }
 
-        console.log('JB personAndDOgs', personAndDogs)
         const firstCdo = await getCdo(personAndDogs.dogs[0].indexNumber, user)
-        console.log('JB firstCdo', firstCdo)
 
         const breadcrumbLink = getMainReturnPoint(request)
 
