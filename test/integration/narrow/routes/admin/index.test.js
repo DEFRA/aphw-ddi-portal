ff7fd9ba2132b76bc5bef1a551b03b8c411f7748
Chris Cole <whitewaterdesign>
2024-11-12 09:27:38 +0000

    feat: 451608 - Add link on admin page to police officer list

--- test/integration/narrow/routes/admin/index.test.js
+++ test/integration/narrow/routes/admin/index.test.js
@@ -32,12 +32,15 @@ describe('Admin index', () => {
     const { document } = new JSDOM(response.payload).window
 
     expect(response.statusCode).toBe(200)
+    const mainContentTextContent = document.querySelector('#main-content').textContent.trim()
+
     expect(document.querySelectorAll('h1.govuk-heading-l')[0].textContent.trim()).toBe('Admin area')
-    expect(document.querySelector('#main-content').textContent.trim()).toContain('Manage Index lists')
-    expect(document.querySelector('#main-content').textContent.trim()).toContain('Courts')
-    expect(document.querySelector('#main-content').textContent.trim()).toContain('Police forces')
-    expect(document.querySelector('#main-content').textContent.trim()).toContain('Dog insurers')
-    expect(document.querySelector('#main-content').textContent.trim()).toContain('Delete dog owner records without a dog')
-    expect(document.querySelector('#main-content').textContent.trim()).toContain('Add or remove police officers')
+    expect(mainContentTextContent).toContain('Manage Index lists')
+    expect(mainContentTextContent).toContain('Courts')
+    expect(mainContentTextContent).toContain('Police forces')
+    expect(mainContentTextContent).toContain('Dog insurers')
+    expect(mainContentTextContent).toContain('Delete dog owner records without a dog')
+    expect(mainContentTextContent).toContain('Add or remove police officers')
+    expect(mainContentTextContent).toContain('List police officer access')
   })
 })
