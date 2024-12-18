b53be19f2c503267bc88bb512ef2815d5d67f4aa
work <jeremy.barnsley@esynergy.co.uk>
2024-12-18 14:14:45 +0000

    Fixed lint

--- app/models/mappers/check-history.js
+++ app/models/mappers/check-history.js
@@ -188,14 +188,15 @@ const getActivityLabelFromEvent = (event) => {
     return 'NOT YET DEFINED'
   }
 
-  // Special case - for Form 2 sent
-  if (event.activity?.activityType === 'sent' && event.activity?.activityLabel && event.activity.activityLabel.startsWith('Form 2 from ')) {
-    return `${event.activity?.activityLabel} requested`
+  if (event.activity?.activityType) {
+    // Special case - for Form 2 sent
+    if (event.activity.activityType === 'sent' && event.activity.activityLabel.startsWith('Form 2 from ')) {
+      return `${event.activity.activityLabel} requested`
+    }
+
+    return `${event.activity?.activityLabel} ${event.activity.activityType}`
   }
 
-  if (event.activity?.activityType) {
-    return `${event.activity?.activityLabel} ${event.activity?.activityType}`
-  }
   return 'NOT YET DEFINED'
 }
 /**
