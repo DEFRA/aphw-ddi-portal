65c120c1916bf260f71bac8509e139d26902e487
work <jeremy.barnsley@esynergy.co.uk>
2025-01-17 14:33:46 +0000

    Extra coverage

--- test/unit/lib/address-helper.test.js
+++ test/unit/lib/address-helper.test.js
@@ -40,37 +40,50 @@ describe('shuffleAddress test', () => {
       ddi_postcode: ''
     })
   })
-
-  describe('shuffleFieldDataIfNeeded test', () => {
-    test('should not shuffle address', () => {
-      expect(shuffleFieldDataIfNeeded({
-        ddi_index_number: 'ED12345',
-        ddi_address_line_1: 'addr1',
-        ddi_address_line_2: 'addr2',
-        ddi_town: 'town',
-        ddi_postcode: 'postcode'
-      })).toEqual({
-        ddi_index_number: 'ED12345',
-        ddi_address_line_1: 'addr1',
-        ddi_address_line_2: 'addr2',
-        ddi_town: 'town',
-        ddi_postcode: 'postcode'
-      })
+  test('should build address when all lines are null', () => {
+    expect(shuffleAddress({
+      ddi_address_line_1: null,
+      ddi_address_line_2: null,
+      ddi_town: null,
+      ddi_postcode: null
+    })).toEqual({
+      ddi_address_line_1: '',
+      ddi_address_line_2: '',
+      ddi_town: '',
+      ddi_postcode: ''
     })
-    test('should shuffle address', () => {
-      expect(shuffleFieldDataIfNeeded({
-        ddi_index_number: 'ED12345',
-        ddi_address_line_1: 'addr1',
-        ddi_address_line_2: '',
-        ddi_town: null,
-        ddi_postcode: 'postcode'
-      })).toEqual({
-        ddi_index_number: 'ED12345',
-        ddi_address_line_1: 'addr1',
-        ddi_address_line_2: 'postcode',
-        ddi_town: '',
-        ddi_postcode: ''
-      })
+  })
+})
+
+describe('shuffleFieldDataIfNeeded test', () => {
+  test('should not shuffle address', () => {
+    expect(shuffleFieldDataIfNeeded({
+      ddi_index_number: 'ED12345',
+      ddi_address_line_1: 'addr1',
+      ddi_address_line_2: 'addr2',
+      ddi_town: 'town',
+      ddi_postcode: 'postcode'
+    })).toEqual({
+      ddi_index_number: 'ED12345',
+      ddi_address_line_1: 'addr1',
+      ddi_address_line_2: 'addr2',
+      ddi_town: 'town',
+      ddi_postcode: 'postcode'
+    })
+  })
+  test('should shuffle address', () => {
+    expect(shuffleFieldDataIfNeeded({
+      ddi_index_number: 'ED12345',
+      ddi_address_line_1: 'addr1',
+      ddi_address_line_2: '',
+      ddi_town: null,
+      ddi_postcode: 'postcode'
+    })).toEqual({
+      ddi_index_number: 'ED12345',
+      ddi_address_line_1: 'addr1',
+      ddi_address_line_2: 'postcode',
+      ddi_town: '',
+      ddi_postcode: ''
     })
   })
 })
